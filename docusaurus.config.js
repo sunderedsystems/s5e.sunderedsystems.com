4ba41bcbac3b2b98d0fc35d642ec3331dabeceb1
unknown <Sundered Admin>
2025-08-06 00:13:28 -0500

    local config changes before merge

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,11 +25,6 @@ const config = {
     locales: ['en'],
   },
 
-  // Remove favicon-switcher.js, you don't need it!
-  // scripts: [
-  //   '/favicon-switcher.js',
-  // ],
-
   presets: [
     [
       'classic',
@@ -45,79 +40,103 @@ const config = {
     ],
   ],
 
-  themeConfig:
-    ({
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Home',
-        logo: {
-          alt: 'Sundered logo',
-          // LIGHT MODE LOGO
-          src: 'img/inv.logo.png',         // <--- black/dark logo for light mode
-          // DARK MODE LOGO
-          srcDark: 'img/logo.png',         // <--- white/light logo for dark mode
-          href: '/',
+  themeConfig: ({
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'Sundered logo',
+        // LIGHT MODE LOGO
+        src: 'img/inv.logo.png',
+        // DARK MODE LOGO
+        srcDark: 'img/logo.png',
+        href: '/',
+      },
+      items: [
+        {
+          label: 's5e Rules',
+          to: '/docs/welcome',
+          position: 'left',
         },
-        items: [
-          {
-            label: 's5e Rules',
-            to: '/docs/welcome',
-            position: 'left',
-          },
-          {
-            label: 'Sundered Creator License',
-            to: '/docs/SCL',
-            position: 'left',
-          },
-          {
-            href: 'https://github.com/sunderedsystems/sundered5e',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+        {
+          label: 'Sundered Creator License',
+          to: '/docs/SCL',
+          position: 'left',
+        },
+        {
+          href: 'https://core.sunderedsystems.com',
+          label: 'Sundered Core',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/sunderedsystems/sundered5e',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/sunderedhttps://discord.gg/wMJ2xjb6',
+            },
+            {
+              label: 'TikTok',
+              href: 'https://www.tiktok.com/@sunderedsystems',
+            },
+            {
+              label: 'Instagram',
+              href: 'https://www.instagram.com/sunderedsystems',
+            },
+            {
+              label: 'Reddit',
+              href: 'https://www.reddit.com/r/sunderedsystems',
+            },
+          ],
+        },
+        {
+          title: 'Legal',
+          items: [
+            {
+              label: 'Sundered Creator License',
+              to: '/docs/SCL',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/sunderedsystems/sundered5e',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Sundered Systems.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  }),
+
+  plugins: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        hashed: true,
+        // language: ['en'],
+        // docsRouteBasePath: ['docs'],
+        // searchBarShortcutKeymap: 'ctrl+shift+f',
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/sunderedhttps://discord.gg/wMJ2xjb6',
-              },
-              {
-                label: 'Twitter / X',
-                href: 'https://x.com/sunderedsystem',
-              },
-            ],
-          },
-          {
-            title: 'Legal',
-            items: [
-              {
-                label: 'Sundered Creator License',
-                to: '/docs/SCL',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/sunderedsystems/sundered5e',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Sundered Systems.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+    ],
+  ],
 };
 
 export default config;
